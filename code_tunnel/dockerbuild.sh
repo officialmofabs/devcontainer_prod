665a47433e8f3f6ab4d04017bd9598a9bb184b7f
officialmofabs <officialmofabs@outlook.com>
2025-07-03 05:31:27 +0100

    master commiy

--- code_tunnel/dockerbuild.sh
+++ code_tunnel/dockerbuild.sh
@@ -2,5 +2,6 @@
 docker build \
   --build-arg USER_ID="$(id -u)" \
   --build-arg GROUP_ID="$(id -g)" \
-  --build-arg DOCKER_GROUP_ID="$(getent group docker | cut -d ':' -f 3)" \
+  --build-arg DOCKER_GROUP_ID="968"
+  #(getent group docker | cut -d ':' -f 3)" \
   -t nonroot-devcontainer .
